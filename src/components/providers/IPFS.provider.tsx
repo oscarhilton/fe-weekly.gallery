da615239b65b363633b348f8565adfc6d65eb970
Oscar Hilton <oscar.hilton@hyd.agency>
2021-10-01 20:06:44 +0100

    Peer connection using simple peer but they are both sending to each other, one needs to be leader:

--- src/components/providers/IPFS.provider.tsx
+++ src/components/providers/IPFS.provider.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import Peer from 'simple-peer';
 
 interface context {
 	updateDatabase: (key: string, payload: any) => void;
 }
 
 interface clientUser {
+	name: string;
+	avatar: string;
   x: number;
   y: number;
 }
@@ -30,58 +34,112 @@ const IPFS_CONFIG = {
 	}
 };
 
+const SIMPLE_PEER_CONFIG = {
+  channelConfig: {},
+  channelName: 'testingthewater',
+  config: { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }, { urls: 'stun:global.stun.twilio.com:3478?transport=udp' }] },
+  offerOptions: {},
+  answerOptions: {},
+  sdpTransform: function (sdp: any) { return sdp },
+  streams: [],
+  trickle: true,
+  allowHalfTrickle: false,
+  // wrtc: {}, // RTCPeerConnection/RTCSessionDescription/RTCIceCandidate
+}
+
 export const IPFSContext = React.createContext<context>({ updateDatabase: () => {} });
 
 export default function IPFSProvider({ children }: { children: any }) {
   const [loaded, setLoaded] = React.useState<number>(0);
-  const [userData, setUserData] = React.useState<clientUser>();
+  const [userData, setUserData] = React.useState([]);
   const [currentHash, setCurrentHash] = React.useState<string>("");
+	const [userProfileInstance, setUserProfileInstance] = React.useState<any | null>(null);
   const [ipfsConntected, setIpfsConnected] = React.useState<boolean>(false);
   const [orbitDbConntected, setOrbitDbConnected] = React.useState<boolean>(false);
   const [activeUsers, setActiveUsers] = React.useState<number>(0);
 
+	const { register, handleSubmit } = useForm(); 
+
   React.useEffect(() => {
     (async () => {
-      const ipfs = await window.Ipfs.create({
+			const ipfs = await window.Ipfs.create({
 				repo: "/weekly.gallery.ipfs",
 				...IPFS_CONFIG
 			});
-      setIpfsConnected(!!ipfs);
-      const orbitdb = await window.OrbitDB.createInstance(ipfs);
+			setIpfsConnected(!!ipfs);
 
-      const options = {
-        maxHistory: 1,
-        accessController: {
-          write: ["*"]
-        },
-        onLoad: () => setOrbitDbConnected(orbitdb.id),
-      };
+			const orbitdb = await window.OrbitDB.createInstance(ipfs);
 
-      // ACTIVE USERS DB
-      const activeUsersCounter = await orbitdb.counter(
+			console.log(orbitdb.identity.publicKey);
+
+			const options = {
+				maxHistory: 1,
+				accessController: {
+					write: ["*"]
+				},
+			};
+
+			setOrbitDbConnected(orbitdb.id);
+
+			// ACTIVE USERS DB
+			const swarmDatabase = await orbitdb.docs(MAIN_DB_NAME + "swarm", options);
+
+			swarmDatabase.load();
+
+			swarmDatabase.events.on("replicated", async () => {
+				const peer = new Peer({ ...SIMPLE_PEER_CONFIG, initiator: true }) as any;
+				peer.on(
+					"signal",
+					async (signal: any) => {
+						await swarmDatabase.put({
+							_id: orbitdb.identity.publicKey,
+							signal,
+							peer: peer._id
+						});
+				})
+				peer.on("connect", () => {
+					// wait for 'connect' event before using the data channel
+					peer.send("hey peer, how is it going?" + Math.random());
+				});
+
+				peer.on("data", (data: any) => {
+					// got a data channel message
+					console.log("got a message from peer: " + data);
+				});
+				const others = await swarmDatabase.query(
+					(peer: any) => peer._id !== orbitdb.identity.publicKey
+				);
+				others.forEach((other: any) => {
+					peer.signal(other.signal);
+				})
+			});
+
+			// ACTIVE USERS DB
+			const activeUsersCounter = await orbitdb.counter(
 				MAIN_DB_NAME + "users.total",
 				options
 			);
 
-      activeUsersCounter.events.on("ready", async () => {
-        await activeUsersCounter.inc(2000);
-        updateActiveUsersUI(activeUsersCounter.value);
-      });
-      activeUsersCounter.events.on("replicated", () => updateActiveUsersUI(activeUsersCounter.value));
-      activeUsersCounter.events.on("write", () => updateActiveUsersUI(activeUsersCounter.value));
-      activeUsersCounter.events.on('load.progress', (address: any, hash: any, entry: any, progress: any, total: any) =>  {
-        setCurrentHash(hash);
-        setLoaded(progress / total)
-      });
 			activeUsersCounter.load();
 
-      // USERS DB
-
-
-    })();
+			// USER PROFILE
+			const userProfileDatabase = await orbitdb.docs(MAIN_DB_NAME + "profile", {
+				...options,
+				maxHistory: 10
+			});
+			userProfileDatabase.events.on("ready", async () => {
+				const data = await userProfileDatabase.get("");
+				setUserData(data);
+			});
+			setUserProfileInstance(userProfileDatabase);
+			await userProfileDatabase.load();
+		})();
   }, []);
 
-  const updateActiveUsersUI = (value: number) => setActiveUsers(value);
+	const handleNewEntry = React.useCallback(async (data: any) => {
+		if (!userProfileInstance) return;
+		await userProfileInstance.put({ _id: new Date().getTime(), text: data.newEntry });
+	}, [userProfileInstance]);
 
   return (
 		<IPFSContext.Provider
@@ -96,7 +154,11 @@ export default function IPFSProvider({ children }: { children: any }) {
 				<li>IPFS: {ipfsConntected.toString()}</li>
 				<li>DATABASE: {orbitDbConntected.toString()}</li>
 			</ul>
-			<input />
+			<form onSubmit={handleSubmit(handleNewEntry)}>
+				<input {...register("newEntry", { required: true })} />
+				<button type='submit'>Submit</button>
+			</form>
+			{userData && userData.map(({ text, id }: { text: string, id: string }) => <div key={id}>{text}</div>)}
 			{/* {children} */}
 		</IPFSContext.Provider>
 	);
